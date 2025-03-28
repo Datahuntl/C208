64b25a04e27543eef9eaeafb5278c28c20cdbbdd
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2025-03-27 22:10:05 -0300

    Create EX02.02.asm

---
+++ LABORATORIO/RELATORIO04/EX02.02.asm
@@ -0,0 +1,16 @@
+.text
+
+addi $t1,$t1,20 # valor de A
+addi $t2,$t2,126 # valor de B
+addi $t3,$t3,30 # valor de C
+
+bne $t1,$t2,igual
+	add $t2,$t1,$t3
+	sub $t3,$t2,$t3
+	j fim
+
+igual:
+	add $t3,$t1,$t2
+	sub $t1,$t2,$t3
+
+fim:
