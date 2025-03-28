5dcaf341119df6a58003391b0ec8449a87c081ef
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2025-03-27 21:53:33 -0300

    Create EX01.01.asm

---
+++ LABORATORIO/RELATORIO04/EX01.01.asm
@@ -0,0 +1,16 @@
+.text
+
+addi $t1,$t1,10
+addi $t2,$t2,25
+addi $t3,$t3,43
+addi $t4,$t4,89
+
+srl $s1,$t1,8
+srl $s2,$t2,8
+srl $s3,$t3,8
+srl $s4,$t4,8
+
+sll $a1,$t1,4
+sll $a2,$t2,4
+sll $s3,$t3,4
+sll $s4,$t4,4
